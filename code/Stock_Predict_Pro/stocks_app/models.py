40a701a2cad04770945d9f3e3c19bcdd380706a6
xixiking <BrowningBeljuniorxwVyDdc@gmail.com>
2023-12-03 14:06:29 +0800

    实现了前后端的分离
    解决了前端跨域访问后端API的问题，前端端口是8080，后端端口是8000，另外需要着重注意的是，需要将MYSQL的密码修改回原来的那个，因为我用了我自己的数据库，记得记得改回来，其他都不变

--- code/Stock_Predict_Pro/stocks_app/models.py
+++ code/Stock_Predict_Pro/stocks_app/models.py
@@ -3,6 +3,7 @@
 from django.db import models
 from users_app.models import User  # 从users_app导入User模型
 
+
 class Stocks(models.Model):
     stock_symbol = models.CharField(max_length=10, primary_key=True)  # 股票代码，字符型，最大长度为10，主键
     company_name = models.CharField(max_length=255)  # 公司名称，字符型，最大长度为255
@@ -12,6 +13,7 @@ class Stocks(models.Model):
 
     def __str__(self):
         return self.stock_symbol
+
 
 class Prices(models.Model):
     record_id = models.AutoField(primary_key=True)  # 记录ID，自增主键
@@ -24,6 +26,7 @@ class Prices(models.Model):
     volume = models.PositiveIntegerField()  # 成交量，正整数
     adjusted_close = models.DecimalField(max_digits=10, decimal_places=2)  # 调整后的收盘价，十进制数，最大位数为10，小数位数为2
 
+
 class TechnicalIndicators(models.Model):
     record_id = models.AutoField(primary_key=True)  # 记录ID，自增主键
     stock_symbol = models.ForeignKey(Stocks, on_delete=models.CASCADE)  # 外键链接到Stocks表
@@ -32,12 +35,15 @@ class TechnicalIndicators(models.Model):
     relative_strength_index = models.DecimalField(max_digits=10, decimal_places=2)  # 相对强弱指数，十进制数，最大位数为10，小数位数为2
     stochastic_oscillator = models.DecimalField(max_digits=10, decimal_places=2)  # 随机指标，十进制数，最大位数为10，小数位数为2
 
+
 class News(models.Model):
     record_id = models.AutoField(primary_key=True)  # 记录ID，自增主键
     stock_symbol = models.ForeignKey(Stocks, on_delete=models.CASCADE)  # 外键链接到Stocks表
     timestamp = models.DateTimeField()  # 时间戳，日期时间型
     news_title = models.CharField(max_length=255)  # 新闻标题，字符型，最大长度为255
     news_content = models.TextField()  # 新闻内容，文本型
+    objects = models.Manager()
+
 
 class UserSubscriptions(models.Model):
     subscription_id = models.AutoField(primary_key=True)  # 订阅ID，自增主键
@@ -46,6 +52,7 @@ class UserSubscriptions(models.Model):
     subscription_status = models.CharField(max_length=20)  # 订阅状态，字符型，最大长度为20
     notification_preferences = models.CharField(max_length=255)  # 通知偏好设置，字符型，最大长度为255
 
+
 class UserPredictionRequests(models.Model):
     request_id = models.AutoField(primary_key=True)  # 请求ID，自增主键
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # 外键链接到User表
@@ -53,3 +60,12 @@ class UserPredictionRequests(models.Model):
     prediction_interval = models.CharField(max_length=1, choices=[('M', '分钟'), ('H', '小时')])  # 预测时间间隔，字符型，选项为分钟或小时
     request_timestamp = models.DateTimeField()  # 请求时间戳，日期时间型
     prediction_result = models.CharField(max_length=255)  # 预测结果，字符型，最大长度为255
+
+
+class HotStock(models.Model):
+    stock_symbol = models.CharField(max_length=10)
+    company_name = models.CharField(max_length=100)
+    latest_close_price = models.FloatField()
+    change_amount = models.FloatField()
+    change_percentage = models.FloatField()
+    objects = models.Manager()
