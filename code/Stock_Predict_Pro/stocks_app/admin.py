4011127282f99b510d4b4d145b3efb3ed7598cff
xixiking <BrowningBeljuniorxwVyDdc@gmail.com>
2023-12-03 16:46:01 +0800

    Revert "Revert "实现了前后端的分离""
    This reverts commit 3862b958077a78c72e6afe0f3d486d86ed96173b.

--- code/Stock_Predict_Pro/stocks_app/admin.py
+++ code/Stock_Predict_Pro/stocks_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+# admin.py
+from .models import HotStock
+from .models import News
+
+admin.site.register(HotStock)  # 热门股票
+
+admin.site.register(News)  # 股票新闻
--- code/Stock_Predict_Pro/users_app/views.py
+++ code/Stock_Predict_Pro/users_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django import forms
 # Create your views here.
+
+
+class search(forms.Form):
+    search = forms.CharField(
+        label='搜索栏',
+        widget=forms.TextInput(attrs={'placeholder': '请输入想搜索的内容：'}),
+        required=True
+    )
+
+
+def index_users(request):
+    form = search()
+    return render(request, 'users_app/index.html', {'form': form})
